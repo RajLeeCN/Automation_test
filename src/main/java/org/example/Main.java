e475b3077c4684a7ac5e46564ab375a8c586722a
JIANPING_ZHOU <zhoujianpingls@outlook.com>
2024-03-11 14:12:13 -0700

    AutoTest for create project finished.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,65 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+//        System.setProperty("webdriver.chrome.driver", "/Users/lee/Downloads/chromedriver-mac-arm64");
 
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        // Define an explicit wait
 
-        driver.get("https://www.google.com");
+        try {
+            driver.get("https://app.bugbug.io/sign-in/");
+//            driver.wait(3);
+
+            WebElement emailField = driver.findElement(By.name("email"));
+            WebElement passwordField = driver.findElement(By.name("password"));
+            emailField.sendKeys("zhoujianpingzx@gmail.com"); // replace with your actual email
+            passwordField.sendKeys("156897123468Zx!");
+            WebElement signInBtn = driver.findElement(By.tagName("button"));
+            signInBtn.click();
+//test for sign in page
+
+            WebElement newProjectButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[data-testid='ProjectList.NewProjectButton']")));
+            newProjectButton.click();
+            // After sign-in, wait for navigation and ensure the new page is loaded
+
+            WebElement newProjectName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[data-testid='Input']")));
+            newProjectName.sendKeys("newProject");
+            WebElement websiteToTest = driver.findElement(By.name("homepageUrl"));
+            websiteToTest.sendKeys("https://www.google.com");
+            WebElement createProjectBtn = driver.findElement(By.cssSelector("button[data-testid='Button']"));
+            createProjectBtn.click();
+            WebElement createTestBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[data-testid='EditTestModal.SubmitButton']")));
+            createTestBtn.click();
+//            create new project page
+
+//            WebElement startRecordingButton = driver.findElement(By.xpath("//button[.//div[text()='Start recording']]"));
+//            startRecordingButton.click();
+
+
+
+
+
+//            test the creating project page
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        finally {
+//            driver.quit();
+//        }
     }
 }
